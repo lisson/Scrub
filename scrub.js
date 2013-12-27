0afb03b1ed5e3dd4514a2cbda36ec8c7ffa39f1a
andy <yili604@gmail.com>
2013-12-26 17:22:33 -0800

    Cluster algorithm is implemented but bug is present. Nodes with similar content will be duplicated and considered a cluster.

--- scrub.js
+++ scrub.js
@@ -5,8 +5,14 @@ Copyright (c) 2013 Yi LI <yili604@gmail.com>
 */
 
 var isOpen = false;
-var simpleTags = /(IMG|H[2-9]|CODE|DFN|Q)/i;
+var simpleTags = /(IMG|H[2-9]|CODE|DFN|Q|TABLE)/i;
 var inlineTags = /(A|EM|STRONG)/;
+
+function tagRecord(_dom, r)
+{
+	this.dom = _dom;
+	this.ratio = r;
+}
 
 $('body').click(function(e) {
 	$('#scrubextensionif').remove();
@@ -184,27 +190,29 @@ function findContentTagRatio(node)
 	var targetNode = null;
 	var c;
 	var body2 = node.clone();
-	//divs called comment, widget etc. will be ignored.
-	var comment = /.*(comment|advertisement|navigation|menu|disqus_thread|footer|side|reply|respond).*/i;
+	
+	var records = new Array();
+	//divs called comment, etc. will be ignored.
+	var comment = /.*(comment|advertisement|menu|disqus|footer|reply|respond|relate|share).*/i;
 	body2.find('div').each(function() {
 		if(checkRegex(comment, $( this ) ) )
 		{
 			$(this).remove();
 		}
 	});
-	body2.find("script, style, noscript, iframe, input, textarea").remove();
+	body2.find("script, style, noscript, iframe, input, textarea, aside").remove();
 	body2.find("*").each(function() {
 		c = $( this );
 
 		var content = $.trim(c.text()).replace(/\s\s/g, '');
 		
-		var tags = c.find("*").length;
+		var tags = c.find("*").not("br,span,a").length;
 		var CT;
 		if (tags === 0)
 		{
 			//If no other nested tags, all text is considered distributed over 1 tag.
-			//CT = content.length;
 			CT = content.length;
+			//CT = 0;
 		}
 		else
 		{
@@ -215,11 +223,15 @@ function findContentTagRatio(node)
 				targetNode = this;
 				highestCT = CT;
 		}
-		if (CT > 50)
+		if (CT > 0)
 		{
-			printNode( $( this ) );
-			console.log(" " + CT);
-			//console.log( c.text());
+			//printNode( $( this ) );
+			//console.log(" " + CT);
+			if (this.textContent.trim() != this.parentNode.textContent.trim()) {
+				console.log("Pushing new node into records");
+				records.push(new tagRecord(this, CT));
+			}
+			console.log( content );
 		}
 	});
 	console.log("Highest CT: " + highestCT);
@@ -227,7 +239,103 @@ function findContentTagRatio(node)
 	console.log(targetNode.textContent);
 	//console.log( $( targetNode).text());
 	//Take the grand parent of the text
-	return $( targetNode ).parent().parent();
+	//return $( targetNode ).parent().parent();
+	var parent = checkCluster(records);
+	if (parent === null) {
+		console.log("Nothing is found");
+		return null;
+	}
+	return $(parent);
+}
+
+function checkCluster(records)
+{
+	//This should never happen. Just incase things get weird.
+	if (records.length === 0) {
+		return null;
+	}
+	if (records.length < 3) {
+		return records[0].dom;
+	}
+	//var ClusterCT = new Array();
+	var i;
+	//First element only 2 elements.
+	var highestC = records[0].ratio + records[1].ratio;
+	var highestDom = records[0].dom;
+	var index = 0;
+	//ClusterCT.push(new tagRecord(records[0].dom, c));
+	for(i=1;i<records.length-1;i++)
+	{
+		c = records[i-1].ratio + records[i].ratio + records[i+1].ratio;
+		if (c > highestC) {
+			highestC = c;
+			highestDom = records[i].dom;
+			index = i;
+		}
+		//ClusterCT.push(new tagRecord(records[i].dom, c));
+		//console.log(c);
+		//printNode($(records[i].dom));
+	}
+	console.log("Cluster found in: ");
+	printNode($(highestDom));
+	//Find the lowest common parents of the cluster
+	var common1 = findLCP(records[index-1].dom, records[index].dom);
+	//console.log("First LCP");
+	//printNode($(common1));
+	//console.log("Third element:");
+	//printNode($(records[index+1].dom));
+	var common = findLCP(common1, records[index+1].dom);
+	//console.log("LCP");
+	//printNode($(common));
+	return common;
+}
+
+function findLCP(node1, node2) {
+	var visited = new Array();
+	var found = false;
+	var n1 = node1;
+	var n2 = node2;
+	var parent;
+	console.log("n1:\n" + node1.textContent + "\n\n");
+	console.log("n2:\n" + node2.textContent + "\n\n");
+	while(found === false)
+	{
+		
+		//console.log(n1.nodeName + " " + n2.nodeName);
+		if (n1 === n2) {
+			parent = n1;
+			found = true;
+		}
+		else if (exist(visited, n1)) {
+			parent = n1;
+			found = true;
+		}
+		else if (exist(visited, n2)) {
+			parent = n2;
+			found = true;
+		}
+		visited.push(n1);
+		visited.push(n2);
+		if (n1.nodeName != "BODY") {
+			n1 = n1.parentNode;
+		}
+		if (n2.nodeName != "BODY") {
+			n2 = n2.parentNode;
+		}
+	}
+	return parent;
+}
+
+function exist(arr, target)
+{
+	var i;
+	for(i=0;i<arr.length;i++)
+	{
+		if (target === arr[i]) {
+			return true;
+		}
+	}
+	return false;
 }
 
 //Prints id, class and name for the given jQuery object
@@ -237,6 +345,7 @@ function printNode(node)
 				" ID: " + node.attr("id") +
 				" NAME: " + node.attr("name") +
 				" CLASS: " + node.attr("class") );
+	console.log("\n");
 }
 
 function checkRegex(expr, node){
